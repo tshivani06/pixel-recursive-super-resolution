77f8452dd2ecfce4d8bfb60f7160cc9b7c4419c4
Shivani <teegalashivani06@gmail.com>
2019-12-01 04:40:32 -0600

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -11,7 +11,7 @@ flags.DEFINE_string("samples_dir", "samples", "sampled images save path")
 flags.DEFINE_string("imgs_list_path", "data/train.txt", "images list file path")
 
 flags.DEFINE_boolean("use_gpu", True, "whether to use gpu for training")
-flags.DEFINE_integer("device_id", 0, "gpu device id")
+flags.DEFINE_integer("device_id", 1, "gpu device id")
 
 flags.DEFINE_integer("num_epoch", 30, "train epoch num")
 flags.DEFINE_integer("batch_size", 32, "batch_size")
